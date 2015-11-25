5c302df9fba1d13899f929f947d454aa464f81ec
Adam <adamk@users.noreply.github.com>
2015-11-24 20:08:19 -0500

    Update jove.py
    Fixed a few small errors. Removed functions. Improved format

--- jove.py
+++ jove.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-from __future__ import print_function, division
+
 import math
+
 print('Program to flag Jovian Decametric windows')
 month = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
 week = 42.46/360
 pi = 3.141593
 kr = pi / 180
-form = "###  \ \ ##   ##.#     ###      ###    ##.##     \  \\"
 num1 = open('jovrad.txt', 'w')
-yy = int(raw_input(("Year for which predictions are required ")))
+yy = int(input(("Year for which predictions are required ")))
 e = math.trunc(((yy-1) / 100))
+print(e)
 f = 2 - e + math.trunc(e/4)
+print(f)
 jd = math.trunc(365.25 * (yy - 1)) + 1721423 + f + .5
+print(jd)
 d0 = jd - 2435108
+print(d0)
 incr = 0
 dmax = 0
 tx = 0
@@ -21,23 +25,26 @@ yyly = 0
 yylc = 0
 if yy / 400 - math.trunc((yy / 400)) == 0:
     incr = 1
-    yyly = yy / 4 - math.trunc((yy / 4))
-    yylc = yy / 100 - math.trunc((yy / 100))
+    print("in if-1")
+yyly = yy / 4 - math.trunc((yy / 4))
+yylc = yy / 100 - math.trunc((yy / 100))
 if yyly == 0 and yylc != 0:
+    print("in if-2")
     incr = 1
-    ty = 59 + incr
-    dmax = 365 + incr
-    tx = ty + .5
-print("******************************************************", file=num1)
-print("  JOVIAN IO-DECAMETRIC EMISSION PREDICTIONS FOR ",yy, file=num1)
-print("******************************************************", file=num1)
-print("\n", file=num1)
-print("Day   Date   Hr(UT)  Io_Phase   CML   Dist(AU)  Source", file=num1)
-print("\n", file=num1)
+ty = 59 + incr
+dmax = 365 + incr
+tx = ty + .5
+num1.write("******************************************************\n")
+pout = "  JOVIAN IO-DECAMETRIC EMISSION PREDICTIONS FOR " + str(yy) + "\n"
+num1.write(pout)
+num1.write("******************************************************\n")
+num1.write("\n")
+num1.write("Day   Date   Hr(UT)  Io_Phase   CML   Dist(AU)  Source")
+num1.write("\n")
 th = 0
-def compute(d0, th, dmax):
-    global U1, L3, dt, s
-    d = d0 + math.trunc(th / 24)
+
+while int(th / 24) + 1 <= dmax:
+    d = d0 + (th / 24)
     v = (157.0456 + .0011159 * d) % 360
     m = (357.2148 + .9856003 * d) % 360
     n = (94.3455 + .0830853 * d + .33 * math.sin(kr * v)) % 360
@@ -67,23 +74,19 @@ def compute(d0, th, dmax):
     if L3 < 350 and L3 > 300 and U1 < 250 and U1 > 230:
         s = "Io-C"
     if s != "":
-        outdat()
+        dy = math.trunc((th / 24)) + 1
+        h = th - (dy - 1) * 24
+        if(dy > th):
+            m = math.trunc((dy - tx) / 30.6) + 3
+            da = dy - ty - math.trunc((m - 3) * 30.6 + .5)
+        else:
+            m = math.trunc((dy - 1) / 31) + 1
+            da = dy - (m - 1) * 31
+        mn = month[(m-1)]
+#        mn = month[(m-1)*3+1:(m-1)*3-1+3]
+        outstring = "%s  %i  %5.3f  %5.3f  %5.3f  %5.3f  %s\n" % (mn, da, h, U1, L3, dt, s)
+        num1.write(outstring)
     th = th + .5
 
-    while(((th / 24) + 1) < dmax or ((th / 24) + 1) == dmax):
-        print("Program completed - results in file JOVRAD.TXT", file=num1)
-compute(d0, th, dmax)
-
-def outdat(th,tx,ty):
-    dy = math.trunc((th / 24)) + 1
-    h = th = (dy - 1) * 24
-    if(dy > th):
-        m = math.trunc((dy - tx) / 30.6) + 3
-        da = dy - ty - math.trunc((m - 3) * 30.6 + .5)
-    else:
-        m = math.trunc((dy - 1) / 31) + 1
-        da = dy - (m - 1) * 31
-    m = int(m)
-    mn = month[(m-1)*3+1:(m-1)*3-1+3]
-    print(dy, mn, da, h, U1, L3, dt, s, file=num1)
-outdat(th,tx,ty)
+num1.close()
+print("Program Complete  - results in file JOVRAD.TXT")
